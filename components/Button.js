63f2eb069f32fe5c2898d99bf4a4d3197378160b
tanadendere <tanadendere@gmail.com>
2024-09-04 13:55:10 +0200

    Revert "feat: :sparkles: Introduces a model form react native to select an image"
    This reverts commit 7cef3ba94caace7e2de532def971b5784d17de9a.

--- components/Button.js
+++ components/Button.js
@@ -48,7 +48,10 @@ export default function Button({ label, theme, onPress }) {
   }
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={onPress}>
+      <Pressable
+        style={styles.button}
+        onPress={() => alert("You pressed a button.")}
+      >
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
